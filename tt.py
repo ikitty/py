9ab5b978425398fb966f19c7b34dca18d984d71c
ikitty <xkeitty@163.com>
2014-08-23 13:05:20 +0800

    update

--- tt.py
+++ tt.py
@@ -44,6 +44,7 @@ print dir(mm)
 #dict 
 print '\n===== dict test =======\n'
 for key,value in mm.D.items():
+#for k, v in enumerate(list):
 #for key,value in mm.D.iteritems():
     print key, value
 
@@ -273,3 +274,36 @@ def reverseWords(s=''):
         l.reverse()
         return ' '.join(l).strip()
 print reverseWords('a bb   c')
+
+print '\n===== dir access test =======\n'
+
+import os
+def clearBadFile(folder = os.curdir):
+    #print os.path.abspath(folder)
+    #print os.curdir
+    #dirname() returns the first part of the split path:
+    #print os.path.dirname(folder)
+
+    print os.listdir(folder)
+
+    for dirname, dirnames, filenames in os.walk(folder):
+        #for subdirname in dirnames:
+            #print os.path.join(dirname, subdirname)
+
+        for filename in filenames:
+            if  '.DS_Store' != filename:
+                each_file = os.path.join(dirname, filename)
+                each_size = os.path.getsize(each_file)/1000
+                if  each_size < 100:
+                    os.remove(each_file)
+                    print each_file , ' has been deleted.'
+
+
+        # Advanced usage:
+        # editing the 'dirnames' list will stop os.walk() from recursing into there.
+        if '.git' in dirnames:
+            dirnames.remove('.git')
+
+clearBadFile('./pic/')
+
+
