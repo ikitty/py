23955560148382666c6338534debbf18e0e42951
ikitty <xkeitty@163.com>
2014-09-23 21:49:50 +0800

    opt

--- ch.py
+++ ch.py
@@ -1,22 +1,39 @@
 #!/usr/bin/python
-print '\n===== python challenge test =======\n'
+#python challenge code
 
-print '\n===== channel =======\n'
+print '\n===== 6. channel: collect comment of each file in zip =======\n'
 
-import zipfile, re
+def channel():
+    import zipfile, re
+    cmts, count, f = [], '90052', '%s.txt'
+    myfile = zipfile.ZipFile('shannel.zip')
+    rStr = 'Next nothing is (\d+)'
 
-cmts, count, f = [], '90052', '%s.txt'
-myfile = zipfile.ZipFile('channel.zip')
-rStr = 'Next nothing is (\d+)'
+    while True:
+        ff = f % count
+        cmts.append(myfile.getinfo(ff).comment)
+        try:
+            count = re.search(rStr, myfile.read(ff)).group(1)
+        except:
+            print myfile.read(ff)
+            break
+    print ''.join(cmts)
+#channel()
 
-while True:
-    ff = f % count
-    cmts.append(myfile.getinfo(ff).comment)
-    try:
-        count = re.search(rStr, myfile.read(ff)).group(1)
-    except:
-        print myfile.read(ff)
-        break
+print '\n===== 7. oxygen: get info by analysising gray color =======\n'
 
-print ''.join(cmts)
+def oxygen():
+    import re, Image
 
+    i = Image.open("oxygen.png") 
+    row = [i.getpixel((x, 50)) for x in range(0, i.size[0], 7)]
+    ords = [r for r, g, b, a in row if r == g == b]
+
+    print "".join(map(chr, map(int, re.findall("\d+", "".join(map(chr, ords))))))
+#oxygen()
+
+print '\n===== 8. intergrity: bz compress =======\n'
+#todo
+
+print '\n===== 9. good: connect the dots =======\n'
+#todo
