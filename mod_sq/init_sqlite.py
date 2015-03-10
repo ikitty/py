fe64e4f71f22ca831bc40ea91da24acbb45d13ed
ikitty <xkeitty@163.com>
2015-03-10 20:17:39 +0800

    up

--- mod_sq/init_sqlite.py
+++ mod_sq/init_sqlite.py
@@ -2,15 +2,15 @@
 
 import sqlite3
 
-con = sqlite3.connect('todos.db')
+con = sqlite3.connect('test.db')
 cur = con.cursor()
 
 try:
-    cur.execute('CREATE TABLE todos(id integer PRIMARY KEY AUTOINCREMENT, title text,_order integer,done boolean default False);')
+    cur.execute('CREATE TABLE IF NOT EXISTS test (id integer PRIMARY KEY AUTOINCREMENT, title text,done boolean default False);')
     con.commit()
 except Exception as e:
     print e
-cur.execute('INSERT INTO todos(title, _order, done) values("明天下午3点,coding", 1, 0);')
+cur.execute('INSERT INTO test(title,  done) values("明天下午3点,coding",  0);')
 con.commit()
 
 cur.close()
