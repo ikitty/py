f9fcca669279232ee7c3b8ca9315a7e089b37f62
ikitty <xkeitty@163.com>
2015-03-10 20:19:34 +0800

    up

--- server.py
+++ server.py
@@ -1,15 +1,55 @@
 #!/usr/bin/python
-# coding:utf-8
+#coding:utf-8
 
 import mm
 import json
 import web
+import sqlite3
 from mako.template import Template
+
+
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+
+#create
+#c.execute("CREATE TABLE category(id int primary key, name text)" )
+
+#save changes
+#conn.commit()
+
+#insert
+#c.execute("INSERT INTO category VALUES (1, 'test book')");
+#c.execute("INSERT INTO category VALUES (?, ?)", (2, 'test2'));
+
+#insert list
+books = [(3, 'book3'),
+        (4, 'book4'),
+        ]
+
+#c.executemany("INSERT INTO category VALUES (?,?)", books)
+
+c.execute("SELECT * FROM category")
+#fetchone : get one data
+print(c.fetchall())
+#c.execute("SELECT * FROM category WHERE id=3")
+
+c.execute("UPDATE category SET name = 'test modify' WHERE id = 3")
+c.execute("SELECT * FROM category")
+print(c.fetchall())
+
+
+#delete one data
+c.execute("DELETE FROM category WHERE id = 4")
+c.execute("SELECT * FROM category")
+print(c.fetchall())
+
+conn.commit()
+conn.close()
+
 
 urls = (
     '/', 'index',
-    '/man/', 'Man',
-    '/boy/', 'Boy',
+    '/test/', 'Test',
     '/bblib/(.*)', 'source',
 )
 
@@ -23,12 +63,8 @@ class source:
     def GET(self, fname):
         return mm.rfile('bblib/' + fname)
 
-class Boy:
-    def GET(self):
-        return json.dumps({"name":"boyJack", "age": 10})
 
-
-class Man:
+class Test:
     def GET(self):
         return json.dumps({"name":"jack", "age": 10})
 
